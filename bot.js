7becff4dfa29832facd2c31e2a15bd17b39ba902
خالد الرشيدي <42046412+khaliid4@users.noreply.github.com>
2019-01-05 23:40:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,35 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '+' ; 
-
+const prefix = "+"
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+console.log(`سجل الدخول ${client.user.tag}!`);
+console.log('')
+console.log('')
+console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+console.log(`[Start] ${new Date()}`);
+console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+console.log('')
+console.log('╔[════════════════════════════════════]╗');
+console.log(`تسجيل الدخول ل * [ " ${client.user.username} " ]`);
+console.log('')
+console.log('معلومات : ')
+console.log('')
+console.log(`عدد السيرفرات♤ [ " ${client.guilds.size} " ]`);
+console.log(`المستخدمين♡ [ " ${client.users.size} " ]`);
+console.log(`الرومات♧ [ " ${client.channels.size} " ]`);
+console.log('╚[════════════════════════════════════]╝')
+console.log('By Jackeo')
+console.log('╔[════════════]╗')
+console.log(' البوت اون لاين')
+console.log('╚[════════════]╝')
+console.log('V2 العربية arap ')
+console.log('Dev Narox by Jackeo')
+
+
+
+
+
 });
 
 client.on('message', msg => {
